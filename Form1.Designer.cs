ef86885bdacf8d2442db2a6c21f19026096bae01
Skyman2413 <67760761+Skyman2413@users.noreply.github.com>
2020-12-15 04:01:40 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Nekit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c = new System.Windows.Forms.Button();
+            this.d = new System.Windows.Forms.Button();
+            this.en = new System.Windows.Forms.Button();
+            this.f = new System.Windows.Forms.Button();
+            this.g = new System.Windows.Forms.Button();
+            this.a = new System.Windows.Forms.Button();
+            this.b = new System.Windows.Forms.Button();
+            this.c_up = new System.Windows.Forms.Button();
+            this.d_up = new System.Windows.Forms.Button();
+            this.f_up = new System.Windows.Forms.Button();
+            this.g_up = new System.Windows.Forms.Button();
+            this.a_up = new System.Windows.Forms.Button();
+            this.rec = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // c
+            // 
+            this.c.Location = new System.Drawing.Point(33, 251);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(77, 128);
+            this.c.TabIndex = 0;
+            this.c.Text = "C";
+            this.c.UseVisualStyleBackColor = true;
+            this.c.Click += new System.EventHandler(this.c_Click);
+            // 
+            // d
+            // 
+            this.d.Location = new System.Drawing.Point(116, 251);
+            this.d.Name = "d";
+            this.d.Size = new System.Drawing.Size(77, 128);
+            this.d.TabIndex = 1;
+            this.d.Text = "D";
+            this.d.UseVisualStyleBackColor = true;
+            this.d.Click += new System.EventHandler(this.d_Click);
+            // 
+            // en
+            // 
+            this.en.Location = new System.Drawing.Point(199, 251);
+            this.en.Name = "en";
+            this.en.Size = new System.Drawing.Size(77, 128);
+            this.en.TabIndex = 2;
+            this.en.Text = "E";
+            this.en.UseVisualStyleBackColor = true;
+            this.en.Click += new System.EventHandler(this.e_Click);
+            // 
+            // f
+            // 
+            this.f.Location = new System.Drawing.Point(282, 251);
+            this.f.Name = "f";
+            this.f.Size = new System.Drawing.Size(77, 128);
+            this.f.TabIndex = 3;
+            this.f.Text = "F";
+            this.f.UseVisualStyleBackColor = true;
+            this.f.Click += new System.EventHandler(this.f_Click);
+            // 
+            // g
+            // 
+            this.g.Location = new System.Drawing.Point(365, 251);
+            this.g.Name = "g";
+            this.g.Size = new System.Drawing.Size(77, 128);
+            this.g.TabIndex = 4;
+            this.g.Text = "G";
+            this.g.UseVisualStyleBackColor = true;
+            this.g.Click += new System.EventHandler(this.g_Click);
+            // 
+            // a
+            // 
+            this.a.Location = new System.Drawing.Point(448, 251);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(77, 128);
+            this.a.TabIndex = 5;
+            this.a.Text = "A";
+            this.a.UseVisualStyleBackColor = true;
+            this.a.Click += new System.EventHandler(this.a_Click);
+            // 
+            // b
+            // 
+            this.b.Location = new System.Drawing.Point(531, 251);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(77, 128);
+            this.b.TabIndex = 6;
+            this.b.Text = "B";
+            this.b.UseVisualStyleBackColor = true;
+            this.b.Click += new System.EventHandler(this.b_Click);
+            // 
+            // c_up
+            // 
+            this.c_up.Location = new System.Drawing.Point(90, 154);
+            this.c_up.Name = "c_up";
+            this.c_up.Size = new System.Drawing.Size(44, 91);
+            this.c_up.TabIndex = 7;
+            this.c_up.Text = "С#";
+            this.c_up.UseVisualStyleBackColor = true;
+            this.c_up.Click += new System.EventHandler(this.c_up_Click);
+            // 
+            // d_up
+            // 
+            this.d_up.Location = new System.Drawing.Point(173, 154);
+            this.d_up.Name = "d_up";
+            this.d_up.Size = new System.Drawing.Size(44, 91);
+            this.d_up.TabIndex = 8;
+            this.d_up.Text = "D#";
+            this.d_up.UseVisualStyleBackColor = true;
+            this.d_up.Click += new System.EventHandler(this.d_up_Click);
+            // 
+            // f_up
+            // 
+            this.f_up.Location = new System.Drawing.Point(338, 154);
+            this.f_up.Name = "f_up";
+            this.f_up.Size = new System.Drawing.Size(44, 91);
+            this.f_up.TabIndex = 9;
+            this.f_up.Text = "F#";
+            this.f_up.UseVisualStyleBackColor = true;
+            this.f_up.Click += new System.EventHandler(this.f_up_Click);
+            // 
+            // g_up
+            // 
+            this.g_up.Location = new System.Drawing.Point(424, 154);
+            this.g_up.Name = "g_up";
+            this.g_up.Size = new System.Drawing.Size(44, 91);
+            this.g_up.TabIndex = 10;
+            this.g_up.Text = "G#";
+            this.g_up.UseVisualStyleBackColor = true;
+            this.g_up.Click += new System.EventHandler(this.g_up_Click);
+            // 
+            // a_up
+            // 
+            this.a_up.Location = new System.Drawing.Point(509, 154);
+            this.a_up.Name = "a_up";
+            this.a_up.Size = new System.Drawing.Size(44, 91);
+            this.a_up.TabIndex = 11;
+            this.a_up.Text = "A#";
+            this.a_up.UseVisualStyleBackColor = true;
+            this.a_up.Click += new System.EventHandler(this.a_up_Click);
+            // 
+            // rec
+            // 
+            this.rec.Location = new System.Drawing.Point(461, 23);
+            this.rec.Name = "rec";
+            this.rec.Size = new System.Drawing.Size(70, 35);
+            this.rec.TabIndex = 12;
+            this.rec.Text = "Record";
+            this.rec.UseVisualStyleBackColor = true;
+            this.rec.Click += new System.EventHandler(this.rec_Click);
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(461, 73);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(70, 34);
+            this.open.TabIndex = 13;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 400);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.rec);
+            this.Controls.Add(this.a_up);
+            this.Controls.Add(this.g_up);
+            this.Controls.Add(this.f_up);
+            this.Controls.Add(this.d_up);
+            this.Controls.Add(this.c_up);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.g);
+            this.Controls.Add(this.f);
+            this.Controls.Add(this.en);
+            this.Controls.Add(this.d);
+            this.Controls.Add(this.c);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button en;
+
+        private System.Windows.Forms.Button a_up;
+        private System.Windows.Forms.Button c_up;
+        private System.Windows.Forms.Button d_up;
+        private System.Windows.Forms.Button f_up;
+        private System.Windows.Forms.Button g_up;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button rec;
+
+        private System.Windows.Forms.Button a;
+        private System.Windows.Forms.Button b;
+        private System.Windows.Forms.Button c;
+        private System.Windows.Forms.Button d;
+        private System.Windows.Forms.Button f;
+        private System.Windows.Forms.Button g;
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Melanchall.DryWetMidi.Devices;
+using NAudio.Midi;
+
+namespace Nekit
+{
+    public partial class Form1 : Form
+    {
+        private string _inputPath;
+        private MidiOut _midiOut;
+        private bool _isRecord;
+        private MidiEventCollection _coll;
+        private int i;
+        private long startTime;
+        public Form1()
+        {
+            InitializeComponent();
+            _midiOut = new MidiOut(0);
+            _isRecord = false;
+            _coll = new MidiEventCollection(0, 50);
+        }
+
+        private void c_Click(object sender, EventArgs e)
+        {
+            
+            _midiOut.Send(MidiMessage.StartNote(60,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(60,127,1).RawData);
+            if (_isRecord)
+            {
+                
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 60, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 60, 0), 1);
+                i++;            }
+        }
+
+        private void c_up_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(61,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(61,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 61, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 61, 0), 1);
+                i++;            }
+        }
+
+        private void d_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(62,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(62,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 62, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 62, 0), 1);
+                i++;            }
+        }
+
+        private void d_up_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(63,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(63,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 63, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 63, 0), 1);
+                i++;            }
+        }
+
+        private void e_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(64,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(64,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 64, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 64, 0), 1);
+                i++;            }
+        }
+
+        private void f_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(65,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(65,127,1).RawData);
+            if (_isRecord)
+            { 
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 65, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 65, 0), 1);
+                i++;
+            }
+        }
+
+        private void f_up_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(66,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(66,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 66, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 66, 0), 1);
+                i++;            }
+        }
+
+        private void g_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(67,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(67,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 67, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 67, 0), 1);
+                i++;            }
+        }
+
+        private void g_up_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(68,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(68,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 68, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 68, 0), 1);
+                i++;            }
+        }
+
+        private void a_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(69,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(69,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 69, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 69, 0), 1);
+                i++;            }
+        }
+
+        private void a_up_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(70,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(70,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 70, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 70, 0), 1);
+                i++;            }
+        }
+
+        private void b_Click(object sender, EventArgs e)
+        {
+            _midiOut.Send(MidiMessage.StartNote(71,127,1).RawData);
+            Thread.Sleep(100);
+            _midiOut.Send(MidiMessage.StopNote(71,127,1).RawData);
+            if (_isRecord)
+            {
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000,1,
+                    MidiCommandCode.NoteOn, 71, 127),1 );
+                _coll.AddEvent(new NoteEvent((DateTime.Now.Ticks - startTime)/90000, 1,
+                    MidiCommandCode.NoteOff, 71, 0), 1);
+                i++;            }
+        }
+
+        private void rec_Click(object sender, EventArgs e)
+        {
+            rec.Text = "Stop record";
+            if (_isRecord)
+            {
+                MidiFile.Export(@"../../MidiFile" + DateTime.Now.Ticks + ".mid", _coll);
+                _coll = new MidiEventCollection(0, 100);
+                rec.Text = "Record";
+            }
+            _isRecord = !_isRecord;
+            startTime = DateTime.Now.Ticks;
+
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            
+            if(openFileDialog.ShowDialog() == DialogResult.OK){// Тут откроется форма и если будет нажата кнопка ОК тогда путь запишется в переменную path.
+               _inputPath = openFileDialog.FileName;
+            }
+
+            _midiOut.Dispose();
+            _midiOut.Close();
+             try
+            {
+                var midiFile = Melanchall.DryWetMidi.Core.MidiFile.Read(_inputPath);
+                using (var outputDevice = OutputDevice.GetByName("Microsoft GS Wavetable Synth"))
+                {
+                    midiFile.Play(outputDevice);
+                }
+
+            }
+           catch (Exception ee)
+            {
+                MessageBox.Show("Unreadeble file", "error", MessageBoxButtons.OK);
+            }
+            
+            _midiOut = new MidiOut(0);
+
+        }
+    }
+}
